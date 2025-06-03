d608d8759614aed88db938cb37991c3a6032e803
Luis López <lopezpuigluis@fpllefia.com>
2025-06-03 18:19:54 +0200

    pagina movies explorer, header y about

---
+++ src/app/MovieExplorer/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function HomePage() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-4xl font-bold text-center mb-8">
+          Películas y Series
+        </h1>
+      </div>
+    </div>
+  );
+}
---
+++ src/misComponentes/Header.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <header className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto px-4">
+        <h1 className="text-3xl font-bold text-center">
+          Movie Explorer - Luis Lopez Puig
+        </h1>
+        <p className="text-center mt-2">
+          Explora las últimas películas y series de TV
+        </p>
+      </div>
+
+      <nav className="mt-4">
+        <ul className="flex justify-center space-x-6">
+          <li>
+            <Link href="/" className="text-white hover:text-gray-300">
+              Inicio
+            </Link>
+          </li>
+          <li>
+            <Link href="/About" className="text-white hover:text-gray-300">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/MovieExplorer"
+              className="text-white hover:text-gray-300">
+              Explorador de Películas
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
